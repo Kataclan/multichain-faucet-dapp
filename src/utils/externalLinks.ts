569acce83fe2db8bbeaf9fac4f462f3d2a254c7f
AC Guerrero <alex.lauks.developer@pm.me>
2021-08-19 10:35:19 +0200

    fix links

--- src/utils/externalLinks.ts
+++ src/utils/externalLinks.ts
@@ -4,21 +4,33 @@ import { getFaucetAddress, getTokenAddress } from './addressHelpers';
 export const getFaucetContractLink = (chainId: NetworkId) => {
   const address = getFaucetAddress(chainId);
   switch (chainId) {
+    case NetworkId.Eth:
+      return `${process.env.REACT_APP_ETH_SCANNER}/address/${address}`;
+    case NetworkId.Polygon:
+      return `${process.env.REACT_APP_POLYGON_SCANNER}/address/${address}`;
+    case NetworkId.Mumbai:
+      return `${process.env.REACT_APP_MUMBAI_SCANNER}/address/${address}`;
     case NetworkId.Bsc:
-      return `https://bscscan.com/${address}`;
+      return `${process.env.REACT_APP_BSC_SCANNER}/address/${address}`;
     default:
     case NetworkId.BscTestnet:
-      return `https://testnet.bscscan.com/${address}`;
+      return `${process.env.REACT_APP_BSC_TEST_SCANNER}/address/${address}`;
   }
 };
 
 export const getTokenContractLink = (chainId: NetworkId) => {
   const address = getTokenAddress(chainId);
   switch (chainId) {
+    case NetworkId.Eth:
+      return `${process.env.REACT_APP_ETH_SCANNER}/address/${address}`;
+    case NetworkId.Polygon:
+      return `${process.env.REACT_APP_POLYGON_SCANNER}/address/${address}`;
+    case NetworkId.Mumbai:
+      return `${process.env.REACT_APP_MUMBAI_SCANNER}/address/${address}`;
     case NetworkId.Bsc:
-      return `https://bscscan.com/${address}`;
+      return `${process.env.REACT_APP_BSC_SCANNER}/address/${address}`;
     default:
     case NetworkId.BscTestnet:
-      return `https://testnet.bscscan.com/${address}`;
+      return `${process.env.REACT_APP_BSC_TEST_SCANNER}/address/${address}`;
   }
 };
